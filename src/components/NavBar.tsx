6802a8cb009ae35f5d30ba72537b6f061d386cbc
niabezara <niabezarashvili@yahoo.com>
2023-09-15 15:16:31 +0400

    navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div className="flex justify-between items-center p-4  bg-primary-SemiDarkBlue sm:rounded-lg">
+      <Link href={""}>
+        <img src="/images/logo.svg" alt="" className="w-6 sm:w-8" />
+      </Link>
+      <div className="flex space-x-4 items-center sm:space-x-6">
+        <Link href={""}>
+          <img src="/images/icon-nav-home.svg" alt="" />
+        </Link>
+        <Link href={""}>
+          <img src="/images/icon-nav-movies.svg" alt="" />
+        </Link>
+        <Link href={""}>
+          <img src="/images/icon-nav-tv-series.svg" alt="" />
+        </Link>
+        <Link href={""}>
+          <img src="/images/icon-nav-bookmark.svg" alt="" />
+        </Link>
+      </div>
+      <Link href={""}>
+        <img
+          src="/images/image-avatar.png"
+          alt=""
+          className="border border-white rounded-full w-6 sm:w-8"
+        />
+      </Link>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          SemiDarkBlue: "rgba(22, 29, 47, 1)",
+          Red: "rgba(252, 71, 71, 1)",
+          PureWhite: "rgba(255, 255, 255, 1)",
+          GreyishBlue: "rgba(90, 105, 143, 1)",
+          DarkBlue: "rgba(16, 20, 30, 1)",
+          black: "rgba(0, 0, 0, 1)",
+        },
+        customDarkBlue: "var(--Dark-Blue, #10141E)",
+      },
+      screens: {
+        sm: "600px",
+        md: "768px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
