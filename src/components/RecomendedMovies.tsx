72e1150395297ca0c050a97335d55ae429df427b
niabezara <niabezarashvili@yahoo.com>
2023-09-16 20:49:34 +0400

    trendingmovies added

--- src/components/RecomendedMovies.tsx
+++ src/components/RecomendedMovies.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import MovieData from "../data/data.json";
 
 export default function RecomendedMovies() {
-  return <div>RecomendedMovies</div>;
+  return (
+    <div className="m-4">
+      <h1>Recommended for you</h1>
+      <div className="flex flex-wrap w-full h-full gap-4 items-center">
+        {MovieData.map((movies) =>
+          movies.isTrending ? null : (
+            <div
+              className="rounded-lg w-40 h-28 p-3"
+              style={{
+                backgroundImage: `url(${movies.thumbnail.regular.small})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "100% 100%",
+              }}
+            >
+              <p>{movies.title}</p>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
 }
