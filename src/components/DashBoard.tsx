08d81fb8aec1f9427081358bf885215af0cfad68
niabezara <niabezarashvili@yahoo.com>
2023-09-16 19:21:06 +0400

    trendingmovies added

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TrendingMovies from "./TrendingMovies";
 import MovieData from "../data/data.json";
+import RecomendedMovies from "./RecomendedMovies";
 
 export default function DashBoard() {
   return (
     <div>
       <TrendingMovies />
+      <RecomendedMovies />
     </div>
   );
 }
--- src/components/TrendingMovies.tsx
+++ src/components/TrendingMovies.tsx
@@ -15,7 +15,7 @@ export default function TrendingMovies() {
   return (
     <div className="m-0 ml-4">
       <h1 className="text-white font-light text-xl ">Trending</h1>
-      <motion.div ref={carousel} className="cursor-grab overflow-hidden ">
+      <motion.div ref={carousel} className="cursor-grab overflow-hidden mt-4">
         <motion.div
           drag="x"
           dragConstraints={{ right: 0, left: -width }}
@@ -25,7 +25,7 @@ export default function TrendingMovies() {
             <motion.div key={index}>
               {movie.isTrending ? (
                 <div
-                  className="w-60 h-40 px-4"
+                  className="w-60 h-40 p-4 relative"
                   style={{
                     backgroundImage: `url(${movie.thumbnail.trending?.small})`,
                     backgroundRepeat: "no-repeat",
@@ -34,10 +34,23 @@ export default function TrendingMovies() {
                     borderRadius: "8px",
                   }}
                 >
-                  <div className="">
-                    <p className="trending-movie text-white">{movie.title}</p>
-                    <p>{movie.year}</p>
-                    <p>{movie.rating}</p>
+                  <div className="absolute bottom-7 flex text-white items-center space-x-1 ">
+                    <p className="text-sm font-light">{movie.year}</p>
+                    <div className="bg-white rounded-full w-0.5 h-0.5"></div>
+                    <img
+                      src="/images/icon-nav-movies.svg"
+                      alt=""
+                      width={12}
+                      style={{ opacity: "0.75" }}
+                    />
+                    <p className="text-sm font-light">{movie.category}</p>
+                    <div className="bg-white rounded-full w-0.5 h-0.5"></div>
+                    <p className="text-sm font-light">{movie.rating}</p>
+                  </div>
+                  <div>
+                    <p className="trending-movie text-white absolute bottom-2">
+                      {movie.title}
+                    </p>
                   </div>
                 </div>
               ) : null}
