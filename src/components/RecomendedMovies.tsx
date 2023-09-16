abf16891cb674126e6d2dd8578b980eae68f3644
niabezara <niabezarashvili@yahoo.com>
2023-09-16 21:25:14 +0400

    trendingmovies added

--- src/components/RecomendedMovies.tsx
+++ src/components/RecomendedMovies.tsx
@@ -4,16 +4,17 @@ import MovieData from "../data/data.json";
 export default function RecomendedMovies() {
   return (
     <div className="m-4">
-      <h1>Recommended for you</h1>
-      <div className="flex flex-wrap w-full h-full gap-4 items-center">
+      <h1 className="text-white font-light text-xl ">Recommended for you</h1>
+      <div className="flex flex-wrap w-full h-full gap-4 items-center mt-6">
         {MovieData.map((movies) =>
           movies.isTrending ? null : (
             <div
-              className="rounded-lg w-40 h-28 p-3"
+              className="rounded-lg w-custom h-28 p-3"
               style={{
                 backgroundImage: `url(${movies.thumbnail.regular.small})`,
                 backgroundRepeat: "no-repeat",
                 backgroundSize: "100% 100%",
+                width: "47%",
               }}
             >
               <p>{movies.title}</p>
