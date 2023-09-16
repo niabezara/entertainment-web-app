d89df8877d3785efa728bb3000582f71321bd84d
niabezara <niabezarashvili@yahoo.com>
2023-09-16 15:57:32 +0400

    carusel

--- src/components/TrendingMovies.tsx
+++ src/components/TrendingMovies.tsx
@@ -1,26 +1,39 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MovieData from "../data/data.json";
 import { motion } from "framer-motion";
 
 export default function TrendingMovies() {
+  const [width, setWidth] = useState<number>(0);
+  const carousel = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (carousel.current) {
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }
+  }, []);
+
   return (
-    <div>
-      <motion.div className="cursor-grab overflow-hidden m-0 ml-4">
-        <motion.div className="caruselinner flex">
+    <div className="m-0 ml-4">
+      <h1 className="text-white font-light text-xl ">Trending</h1>
+      <motion.div ref={carousel} className="cursor-grab overflow-hidden ">
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="caruselinner flex gap-4"
+        >
           {MovieData.map((movie, index) => (
-            <motion.div key={index} className="">
+            <motion.div key={index}>
               {movie.isTrending ? (
                 <div
+                  className="w-60 h-40 px-4"
                   style={{
                     backgroundImage: `url(${movie.thumbnail.trending?.small})`,
-                    backgroundSize: "cover",
                     backgroundRepeat: "no-repeat",
-                    width: "100%",
-                    height: "100%",
+                    pointerEvents: "none",
+                    backgroundSize: "100% 100%",
+                    borderRadius: "8px",
                   }}
                 >
-                  <h1 className="text-white font-light text-xl">Trending</h1>
                   <div className="">
                     <p className="trending-movie text-white">{movie.title}</p>
                     <p>{movie.year}</p>
