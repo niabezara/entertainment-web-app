164bf44783d42084a9daecd56b76d4a99e2d04b0
niabezara <niabezarashvili@yahoo.com>
2023-09-18 11:54:37 +0400

    recomendedmovies added

--- src/components/RecomendedMovies.tsx
+++ src/components/RecomendedMovies.tsx
@@ -8,16 +8,29 @@ export default function RecomendedMovies() {
       <div className="flex flex-wrap w-full h-full gap-4 items-center mt-6">
         {MovieData.map((movies) =>
           movies.isTrending ? null : (
-            <div
-              className="rounded-lg w-32 h-28 p-3"
-              style={{
-                backgroundImage: `url(${movies.thumbnail.regular.small})`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "100% 100%",
-                // width: "47%",
-              }}
-            >
-              <p>{movies.title}</p>
+            <div>
+              <div
+                className="rounded-lg w-40 h-28 p-3"
+                style={{
+                  backgroundImage: `url(${movies.thumbnail.regular.small})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                }}
+              ></div>
+              <div className=" flex text-gray-400 items-center space-x-1 mt-2">
+                <p className="text-sm font-light">{movies.year}</p>
+                <div className="bg-white rounded-full w-0.5 h-0.5 "></div>
+                <img
+                  src="/images/icon-nav-movies.svg"
+                  alt=""
+                  width={12}
+                  style={{ opacity: "0.75" }}
+                />
+                <p className="text-sm font-light">{movies.category}</p>
+                <div className="bg-white rounded-full w-0.5 h-0.5"></div>
+                <p className="text-sm font-light">{movies.rating}</p>
+              </div>
+              <p className="trending-movie text-white ">{movies.title}</p>
             </div>
           )
         )}
