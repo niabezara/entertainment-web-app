740247a5e9a94c7ac2c40ab8fab5a632c2d22163
niabezara <niabezarashvili@yahoo.com>
2023-09-20 19:03:38 +0400

    changes

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -11,7 +11,7 @@ export default function Search({
       <div className="flex p-4 bg-transparent gap-4">
         <img src="/images/icon-search.svg" alt="" />
         <input
-          className="bg-transparent w-full text-white"
+          className="bg-transparent w-full text-white border-none focus:outline-none"
           type="text"
           placeholder="Search for movies or TV series"
           onChange={handleSearch}
