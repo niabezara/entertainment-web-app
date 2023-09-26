4eb7ca14c25d31fe5d76f9032a87298a0ce71d7b
niabezara <niabezarashvili@yahoo.com>
2023-09-26 17:29:18 +0400

    ui changes

--- src/app/Bookmarks/page.tsx
+++ src/app/Bookmarks/page.tsx
@@ -1,5 +1,10 @@
+import Search from "@/components/Search";
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="xl:mt-9 max-w-5xl ">
+      <Search />
+    </div>
+  );
 }
